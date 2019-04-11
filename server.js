0d8235418b7a2099eee69ee9504e47d8047f8e22
emily baier <emilyrbaier@gmail.com>
2019-04-10 17:33:35 -0700

    index, server start

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
-function onRequest(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello World');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    
+    console.log('Server has started.');
 }
-http.createServer(onRequest).listen(8888);
+
+exports.start = start;
