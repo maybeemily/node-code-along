63cf5dfc84d4376176e36dc9f49b61e9c055f696
emily baier <emilyrbaier@gmail.com>
2019-04-13 16:43:13 -0700

    through page 25

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ function start(route, handle) {
         route(handle, pathname);
 
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello World');
+        response.write(content);
         response.end();
     }
     http.createServer(onRequest).listen(8888);
