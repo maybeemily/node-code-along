0dc07d865e7f94d71250fc97dfd83607bc62ead1
emily baier <emilyrbaier@gmail.com>
2019-04-13 17:50:09 -0700

    fixed errors, though page 42

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,20 +4,19 @@ const fs = require('fs');
 function start(response, postData) {
     console.log('Request handler \'start\' was called.');
 
-    const body =  '<html>' +
-    '<head>' +
-    '<meta http-equiv="Content-Type" content="text/html; ' +
-    'charset=UTF-8" />' +
-    '</head>' +
-    '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
-    '</form>' +
-    '</body>' +
-    '</html>';
+    const body = `<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+    <form action="/upload" enctype="multipart/form-data" method="post">
+    <input type="file" name="upload">
+    <input type="submit" value="upload file">
+    </form>
+    </body>
+    </html>`;
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(body);
     response.end();
 }
